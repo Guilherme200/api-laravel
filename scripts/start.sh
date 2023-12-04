15f14441e76d7006128a5a4767b8c4bc1fb15158
Guilherme200 <guimatoso15@gmail.com>
2023-12-04 10:22:42 -0300

    Configurar banco de dados para testes

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,14 +2,12 @@
 
 if [ -f .env ]; then
   source .env
-  source environments/local/.env.local
 fi
 
 if [ "$APP_ENV" == "local" ]; then
   docker compose \
    -f docker-compose.yml \
    -f environments/local/docker-compose.dev.yml \
-   --env-file environments/local/.env.local \
    up --build --remove-orphans --force-recreate -d
 else
   docker compose up --build --remove-orphans --force-recreate -d
